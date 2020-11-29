09741a971f4e685522d3738059eaed0e5ffd33fb
zack <zchiang@g.harvard.edu>
2020-11-28 19:14:18 -0500

    update registration scripts

--- image/helpers/registration/simple_offset_xyz.m
+++ image/helpers/registration/simple_offset_xyz.m
@@ -1,23 +1,29 @@
+% Author: Zachary Chiang, Buenrostro Lab, Harvard University
+% Uses cross correlation to register one stack to another in x, y, and z
+
 function[stackBr] = simple_offset_xyz(stackA, stackB)
     
+    % handle stacks with different numbers of z-planes
+
     if size(stackA,3) < size(stackB,3)
-        new_stackA = uint16(zeros(size(stackB)));
+        new_stackA = zeros(size(stackB),'uint16');
         new_stackA(:,:,1:size(stackA,3)) = stackA;
         stackA = new_stackA;
         stackA(stackA == 0) = median(stackA(:));
         clearvars new_stackA
     end
 
-    stackBr = uint16(zeros(size(stackB)));
+    stackBr = zeros(size(stackB),'uint16');
 
+    % take maximum intensity projections
+    
     imgA = squeeze(max(max(stackA,[],3),[],4));
     imgB = squeeze(max(max(stackB,[],3),[],4));
     
     imgC = squeeze(max(max(stackA,[],2),[],4));
     imgD = squeeze(max(max(stackB,[],2),[],4));
-
-    %figure; imshow(imgC,[0 500])
-    %figure; imshow(imgD,[0 500])
+    
+	% perform cross correlation
     
     c = normxcorr2(imgB, imgA);
     [max_c, imax] = max(abs(c(:)));
@@ -27,11 +33,12 @@ function[stackBr] = simple_offset_xyz(stackA, stackB)
     yoffset = corr_offset(2);
     
     c = normxcorr2(imgD, imgC);
-    %figure; imagesc(c)
     [max_c, imax] = max(abs(c(:)));
     [xpeak, zpeak] = ind2sub(size(c),imax(1));
     corr_offset = [(xpeak-size(imgD,1)) (zpeak-size(imgD,2))];
     zoffset = corr_offset(2);
+    
+	% calculate offset bounds
     
     xbegin_A = max(1+xoffset, 1);
     xend_A   = min(size(stackB,2), size(stackB,2)+xoffset);
@@ -47,7 +54,9 @@ function[stackBr] = simple_offset_xyz(stackA, stackB)
     zend_A   = min(size(stackB,3), size(stackB,3)+zoffset);
     zbegin_B = max(1-zoffset, 1);
     zend_B   = min(size(stackB,3), size(stackB,3)-zoffset);
-           
+
+    % apply offsets    
+    
     stackBr(ybegin_A:yend_A,xbegin_A:xend_A,zbegin_A:zend_A,:) = stackB(ybegin_B:yend_B,xbegin_B:xend_B,zbegin_B:zend_B,:);
     stackBc(stackBr == 0) = median(stackB(:));
     
---
+++ image/helpers/umi_matching/match_spot.m
@@ -0,0 +1,38 @@
+% Author: Zachary Chiang, Buenrostro Lab, Harvard University
+% Given an in situ signal matrix, finds the closest matching ex situ UMI
+% using a probablistic approach
+
+function[match_ind match_val hamming_dist close_umis] = match_spot(curr_umi,umis,intensity_mat,hamming_thresh)
+
+    % fill out Hamming distance matrix
+    
+    hamming = zeros(length(umis),1);
+    for j=1:length(curr_umi)
+        hamming = hamming + (umis(:,j)~=curr_umi(j));
+    end
+
+    % find UMIs with Hamming distance less than threshold
+    
+    close_umi_indices = find(hamming<=hamming_thresh); k=1;
+    while size(close_umi_indices,1) == 0
+        close_umi_indices = find(hamming<=hamming_thresh+k); k=k+1;
+    end
+    close_umis = umis(close_umi_indices,:);
+    
+    % calculate probabilities of close UMIs
+    
+    probs = ones(size(close_umis,1),1)';
+    for j=1:length(curr_umi)
+        bases = str2num(close_umis(:,j));
+        probs = probs.*(intensity_mat(bases,j+1).^2)';
+    end
+
+    % find most likely UMI(s)
+    
+    [min_val min_ind] = min(-log10(probs));
+    
+    match_ind = close_umi_indices(min_ind);
+    match_val = min_val;
+    hamming_dist = hamming(close_umi_indices(min_ind));
+
+end
